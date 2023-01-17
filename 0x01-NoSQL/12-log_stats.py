9c6bb8768dcb97c36e496f91dd5d301c8b1135ea
Ayomide Suara <aysuarex@gmail.com>
2023-01-18 00:08:39 +0100

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
-"""
-script that provides some stats about Nginx logs stored in MongoDB
-"""
+"""Log stat"""
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def logs(nginx):
+    '''
+    Prints Nginx request logs.
+    '''
+    print(f'{nginx.count_documents({})} logs')
+    print('Methods:')
+    reqs = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for req in reqs:
+        print(f'\tmethod {req}: {nginx.count_documents({"method": req})}')
+    print(f'{nginx.count_documents({"path": "/status"})} status check')
+
+
+def conn():
+    '''
+    Establish a connection with MongoDB.
+    '''
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_logs = client.logs.nginx
-    # get number of documents in collection
-    docs_num = nginx_logs.count_documents({})
-    get_num = nginx_logs.count_documents({'method': 'GET'})
-    post_num = nginx_logs.count_documents({'method': 'POST'})
-    put_num = nginx_logs.count_documents({'method': 'PUT'})
-    patch_num = nginx_logs.count_documents({'method': 'PATCH'})
-    delete_num = nginx_logs.count_documents({'method': 'DELETE'})
-    get_status = nginx_logs.count_documents({'method': 'GET',
-                                             'path': '/status'})
-    print("{} logs".format(docs_num))
-    print("Methods:")
-    print("\tmethod GET: {}".format(get_num))
-    print("\tmethod POST: {}".format(post_num))
-    print("\tmethod PUT: {}".format(put_num))
-    print("\tmethod PATCH: {}".format(patch_num))
-    print("\tmethod DELETE: {}".format(delete_num))
-    print("{} status check".format(get_status))
+    nginx = client.logs.nginx
+    logs(nginx)
+
+
+if __name__ == '__main__':
+    conn()
